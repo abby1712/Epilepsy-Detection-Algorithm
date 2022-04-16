aa67e9d91aa8c392bd7e45e6e1885a062e783f27
Abhishek Sebastian <72368959+abby1712@users.noreply.github.com>
2022-04-16 12:57:56 +0530

    Add files via upload

---
+++ Epilepsy_Algo.ino.ino
@@ -0,0 +1,138 @@
+#include <Wire.h>
+#include <arduino-timer.h>
+// MPU6050 Slave Device Address
+const uint8_t MPU6050SlaveAddress = 0x68;
+int Counter = 0;
+double Ax, Ay, Az, T, Gx, Gy, Gz;
+double Ax_P = 0, Ay_P = 0, Az_P = 0;
+
+
+bool reset_counter(void *) {
+  Counter = 0;
+  return true; // repeat? true
+}
+
+// Select SDA and SCL pins for I2C communication
+const uint8_t scl = D6;
+const uint8_t sda = D7;
+
+// sensitivity scale factor respective to full scale setting provided in datasheet
+const uint16_t AccelScaleFactor = 16384;
+const uint16_t GyroScaleFactor = 131;
+
+// MPU6050 few configuration register addresses
+const uint8_t MPU6050_REGISTER_SMPLRT_DIV   =  0x19;
+const uint8_t MPU6050_REGISTER_USER_CTRL    =  0x6A;
+const uint8_t MPU6050_REGISTER_PWR_MGMT_1   =  0x6B;
+const uint8_t MPU6050_REGISTER_PWR_MGMT_2   =  0x6C;
+const uint8_t MPU6050_REGISTER_CONFIG       =  0x1A;
+const uint8_t MPU6050_REGISTER_GYRO_CONFIG  =  0x1B;
+const uint8_t MPU6050_REGISTER_ACCEL_CONFIG =  0x1C;
+const uint8_t MPU6050_REGISTER_FIFO_EN      =  0x23;
+const uint8_t MPU6050_REGISTER_INT_ENABLE   =  0x38;
+const uint8_t MPU6050_REGISTER_ACCEL_XOUT_H =  0x3B;
+const uint8_t MPU6050_REGISTER_SIGNAL_PATH_RESET  = 0x68;
+auto timer = timer_create_default(); // create a timer with default settings
+int16_t AccelX, AccelY, AccelZ, Temperature, GyroX, GyroY, GyroZ;
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin(sda, scl);
+  MPU6050_Init();
+  timer.every(60000, reset_counter);
+
+}
+
+void loop() {
+
+  timer.tick(); // tick the timer
+
+  Read_RawValue(MPU6050SlaveAddress, MPU6050_REGISTER_ACCEL_XOUT_H);
+
+  //divide each with their sensitivity scale factor
+  Ax = (double)AccelX / AccelScaleFactor;
+  Ay = (double)AccelY / AccelScaleFactor;
+  Az = (double)AccelZ / AccelScaleFactor;
+  T = (double)Temperature / 340 + 36.53; //temperature formula
+  Gx = (double)GyroX / GyroScaleFactor;
+  Gy = (double)GyroY / GyroScaleFactor;
+  Gz = (double)GyroZ / GyroScaleFactor;
+
+  Serial.print("Ax: "); Serial.print(Ax);
+  Serial.print(" Ay: "); Serial.print(Ay);
+  Serial.print(" Az: "); Serial.print(Az);
+  Serial.print(" T: "); Serial.print(T);
+  Serial.print(" Gx: "); Serial.print(Gx);
+  Serial.print(" Gy: "); Serial.print(Gy);
+  Serial.print(" Gz: "); Serial.println(Gz);
+
+  Serial.print(" Ax_P-Ax "); Serial.println(abs(Ax_P - Ax));
+  Serial.print(" Ay_P-Ay "); Serial.println(abs(Ay_P - Ay));
+  Serial.print(" Az_P-Az "); Serial.println(abs(Az_P - Az));
+
+  if (abs(Ax_P - Ax) < 0.20 & abs(Ay_P - Ay) < 0.20 & abs(Az_P - Az) < 0.20)
+  {
+    Serial.print("Hand is in Rest");
+  }
+
+
+  
+
+  if (abs(Ax_P - Ax) > 0.80 || abs(Ay_P - Ay) > 0.80 || abs(Az_P - Az) > 0.80)
+  {
+    Counter ++;
+    Serial.print("Hand is in Motion");
+    Serial.print("Counter=");
+    Serial.print(Counter);
+   
+    
+    if (Counter >= 18)
+    { Serial.print(" Alert!");
+      Serial.print("  ");
+      Serial.print(" Possible Epilepsy");
+      Counter = 0;
+    }
+  }
+  Ax_P = Ax; Ay_P = Ay; Az_P = Az;
+
+  delay(1000);
+}
+
+void I2C_Write(uint8_t deviceAddress, uint8_t regAddress, uint8_t data) {
+  Wire.beginTransmission(deviceAddress);
+  Wire.write(regAddress);
+  Wire.write(data);
+  Wire.endTransmission();
+}
+
+// read all 14 register
+void Read_RawValue(uint8_t deviceAddress, uint8_t regAddress) {
+  Wire.beginTransmission(deviceAddress);
+  Wire.write(regAddress);
+  Wire.endTransmission();
+  Wire.requestFrom(deviceAddress, (uint8_t)14);
+  AccelX = (((int16_t)Wire.read() << 8) | Wire.read());
+
+  AccelY = (((int16_t)Wire.read() << 8) | Wire.read());
+  AccelZ = (((int16_t)Wire.read() << 8) | Wire.read());
+  Temperature = (((int16_t)Wire.read() << 8) | Wire.read());
+  GyroX = (((int16_t)Wire.read() << 8) | Wire.read());
+  GyroY = (((int16_t)Wire.read() << 8) | Wire.read());
+  GyroZ = (((int16_t)Wire.read() << 8) | Wire.read());
+}
+
+
+//configure MPU6050
+void MPU6050_Init() {
+  delay(150);
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_SMPLRT_DIV, 0x07);
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_PWR_MGMT_1, 0x01);
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_PWR_MGMT_2, 0x00);
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_CONFIG, 0x00);
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_GYRO_CONFIG, 0x00);//set +/-250 degree/second full scale
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_ACCEL_CONFIG, 0x00);// set +/- 2g full scale
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_FIFO_EN, 0x00);
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_INT_ENABLE, 0x01);
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_SIGNAL_PATH_RESET, 0x00);
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_USER_CTRL, 0x00);
+}
